8cbef59e56fbc93e290df78aac79571436428953
C-Aoudair <aoudair16031999@gmail.com>
2024-08-08 12:40:01 +0100

    test

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+/*import express from 'express';
 import router from './routes/index';
 
 const app = express();
@@ -7,6 +7,21 @@ const port = process.env.PORT || 5000;
 app.use(express.json()); // Middleware to parse JSON request bodies
 
 app.use(router); // Use routes defined in 'routes/index'
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default app;*/
+import express from 'express';
+import controllerRouting from './routes/index';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+controllerRouting(app);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
